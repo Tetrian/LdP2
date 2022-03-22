a2118978797839c51ccd8ddec240c6e455433ced
Tetrian <cerronesimone97@gmail.com>
2022-03-22 21:26:31 +0100

    pag125n16 update

--- pag125n16/B.java
+++ pag125n16/B.java
@@ -1,8 +1,23 @@
-public class B extends Error {
+public class B extends RuntimeException {
 
-  public B() {}
+  public B() {/*
+    public A(double inutile) ci costringe ad aggiungere
+    il costruttore vuoto, poiché fa implicitamente una
+    chiamata super() e quindi senza costruttore vuoto
+    nella superclasse avremmo un errore in compilazione
+  */}
 
-  public B(int x, double y) {}
+public B(int n, double d) {/*
+  per l'espressione x / 2.0 il compilatore fa una promozione
+  implicita a double, da cui il parametro double b
+*/}
 
-  public B(String s) {}
+  public B(String s) {/*
+    il metodo stampa(String s) lancia una eccezione
+    tramite il costruttore della classe B(stringa) da
+    cui ricaviamo che la classe B estende una eccezione,
+    il fatto che il metodo in A non sia definito throws
+    significa che B deve essere di tipo unchecked. Quindi
+    bisogna estendere RuntimeException o Error
+  */}
 }
