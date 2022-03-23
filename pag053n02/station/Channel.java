e9cbac81a8788cdb72be0231c5a2f569c4f131de
Tetrian <cerronesimone97@gmail.com>
2022-03-23 11:55:11 +0100

    pag53n2 aggiunta traccia

---
+++ pag053n02/station/Channel.java
@@ -0,0 +1,12 @@
+
+package station;
+
+class Channel {
+  private String name;
+  private double fm;
+
+  protected Channel(String name, double fm) {
+    this.name = name;
+    this.fm = fm;
+  }
+}
