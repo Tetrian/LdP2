0374e4923c2f16066d8d82b8c069b4808acc59fd
Tetrian <cerronesimone97@gmail.com>
2022-03-28 10:42:34 +0200

    pag062n23 corretto

--- pag062n23/Triangolo.java
+++ pag062n23/Triangolo.java
@@ -1,14 +1,17 @@
 
-import java.lang.Math;
-
 public class Triangolo {
   private double a, b, c;
 
   public Triangolo(double x, double y, double z) {
-    if (x < y + z && y < x + z && z < x + y) {
-      a = x; b = y; c = z;
+    try{
+      if (x < y + z && y < x + z && z < x + y) {
+        a = x; b = y; c = z;
+      }
+      else throw new IllegalArgumentException("incorrect measurements");
     }
-    else throw new RuntimeException("Fail: incorrect measurements");
+    catch (IllegalArgumentException iae){
+      iae.printStackTrace();
+    }
   }
 
   public double getArea() {
@@ -16,13 +19,13 @@ public class Triangolo {
     return Math.sqrt(halfp * (halfp - a) * (halfp - b) * (halfp - c));
   }
 
-  public static class Rettangolo extends Triangolo {
+  static class Rettangolo extends Triangolo {
     public Rettangolo(double c1, double c2) {
       super(c1, c2, Math.sqrt((c1*c1) + (c2*c2)));
     }
   }
 
-  public static class Isoscele extends Triangolo {
+  static class Isoscele extends Triangolo {
     public Isoscele(double base, double side) {
       super(base, side, side);
     }
