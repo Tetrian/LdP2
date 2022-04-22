0da40fd14b7f3d9eee79fc273ec9276f4ada8a9d
Tetrian <cerronesimone97@gmail.com>
2022-04-22 14:54:22 +0200

    cancellazione ridondanze pag062n24

--- pag062n24/Impianto.java
+++ pag062n24/Impianto.java
@@ -1,10 +1,7 @@
 
-import java.util.HashSet;
-import java.util.Iterator;
-
-public class Impianto extends HashSet<Apparecchio> {
+public class Impianto {
   private final int max_watt;
-  private int potenza;
+  private int potenza; // assorbita attualmente dall'impianto
 
   /**
    * Crea un ogetto di tipo impianto con potenza massima (max_watt) pari a
@@ -12,7 +9,6 @@ public class Impianto extends HashSet<Apparecchio> {
    * @param max_watt la potenza massima erogata deve essere positiva
    */
   public Impianto(int max_watt) {
-    super(); // ridondante
     if (max_watt < 0)
       throw new IllegalArgumentException("Valore potenza non valido");
     this.max_watt = max_watt;
@@ -20,22 +16,20 @@ public class Impianto extends HashSet<Apparecchio> {
   }
 
   /**
-   * Aggiunge l'oggetto di tipo Apparecchio passato come argomento nell'HashSet
-   * dell'attuale oggetto Impianto. Inoltre, modifica in modo opportuno gli
-   * attributi dell'oggetto Apparecchio.
+   * Modifica in modo opportuno gli attributi dell'oggetto Apparecchio in modo
+   * da collegarlo a questo impianto.
    * @param nuovo Oggetto di tipo Apparecchio
    * @see Apparecchio
    */
   public void collega(Apparecchio nuovo) {
     nuovo.i_rif = this;   // aggiorno il riferimento di Impianto in Apparecchio
     nuovo.acceso = false; // così da non dover gestire il collegamento di un
-                          // apparecchio accesso;
-    super.add(nuovo);
-  }
+  }                       // apparecchio accesso;
+
 
   /**
    * Ritorna il valore della potenza totale assorbita dagli oggetti di tipo
-   * Apparecchio collegati all'attuale Impianto.
+   * Apparecchio (accesi) collegati all'attuale Impianto.
    * @return this.potenza
    * @see Apparecchio
    */
